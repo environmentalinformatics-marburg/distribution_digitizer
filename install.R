af6f431e43ae14a3079ac1a29a57ada832a8329c
Spaska Forteva <spaska.forteva@geo.uni-marburg.de>
2021-05-20 16:20:40 +0200

    Update install.r

--- install.R
+++ install.R
@@ -13,12 +13,13 @@ library(ShinyImage)
 library(EBImage)
 
 library(shiny)
-options(shiny.host = '0.0.0.0')
+options(shiny.host = '127.0.0.1')
 options(shiny.port = 8888)
 options(shiny.maxRequestSize=100*1024^2)
 runApp('app.R')
 
 
+shiny::runGist("https://gist.github.com/sforteva/138af2ea533c2d1c3d1631b5d2d41e86")
 
 install.packages("BiocManager") 
 BiocManager::install("EBImage")
