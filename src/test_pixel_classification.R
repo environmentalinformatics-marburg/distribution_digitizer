6666a0fcedf072ff319ebe894299650ef526fe54
VenkateshMadhuvanthi <70901714+VenkateshMadhuvanthi@users.noreply.github.com>
2021-05-25 18:45:19 +0200

    Update test_pixel_classification.R

--- src/test_pixel_classification.R
+++ src/test_pixel_classification.R
@@ -11,7 +11,7 @@ py_install(packages = "pillow", pip = FALSE)
 
 setwd("D:/distribution_digitizer_students/")
 source_python("D:/distribution_digitizer_students/src/Pixel_Classification.py")
-workingDir ="D:/distribution_digitizer_students/output/"
+workingDir ="D:/distribution_digitizer_students/"
 n = 5
 m = 9
 mainpixelclassification(workingDir, n, m)
