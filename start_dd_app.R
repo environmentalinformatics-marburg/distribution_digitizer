d6da87e6d43ef6759e9fd2be0b5dc90d25052a7f
dirkzeuss <dirk.zeuss@uni-marburg.de>
2021-06-09 13:23:38 +0200

    paths python and working directory, readme, tidyup

--- start_dd_app.R
+++ start_dd_app.R
@@ -1,13 +1,18 @@
-library(reticulate)
-use_python(Sys.which("python"))
-os <- import("os") 
-py_install(packages = "opencv-python", pip = TRUE)
-py_install(packages = "pillow", pip = FALSE)
-
-setwd("D:/distribution_digitizer_students-main/")
-library(shiny)
-runApp('app.R')
+library(reticulate) # Python binding for R
+use_python(Sys.which("python")) # Set the path to a local python installation
+os <- import("os") # python module needed for managing files, folders and their paths
+py_install(packages = "opencv-python", pip = TRUE) # python module for computer vision
+py_install(packages = "pillow", pip = FALSE) # python module for ...
 
 
-#shiny::runGist("https://gist.github.com/sforteva/138af2ea533c2d1c3d1631b5d2d41e86")
+# Set the path to app.R for being able to execute the shiny app (runApp('app.R'))
+# By default, this app.R lies at the root of this repository. 
+# In RStudio, this path can be set automatically with
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# or manually with a path of your choice:
+# setwd("D:/distribution_digitizer_students-main/") # uncomment this line for setting the working directory manually.
+
+
+library(shiny) # shiny library necessary for starting the app
+runApp('app.R') # the app itself
 
