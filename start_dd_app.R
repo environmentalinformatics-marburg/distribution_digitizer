932496cdda5bfaa40c9c2e42e738d3b72e4b0ec6
VenkateshMadhuvanthi <70901714+VenkateshMadhuvanthi@users.noreply.github.com>
2021-06-09 12:23:05 +0200

    Update start_dd_app.R

--- start_dd_app.R
+++ start_dd_app.R
@@ -1,10 +1,10 @@
 library(reticulate)
+use_python(Sys.which("python"))
 os <- import("os") 
-use_python("C:/ProgramData/Anaconda3/python.exe")
 py_install(packages = "opencv-python", pip = TRUE)
 py_install(packages = "pillow", pip = FALSE)
 
-setwd("D:/distribution_digitizer_students/")
+setwd("D:/distribution_digitizer_students-main/")
 library(shiny)
 runApp('app.R')
 
