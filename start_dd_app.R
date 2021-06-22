c42f8a13d8b1cad2a47922bfece86a44593fe9b3
DirkZeuss <dirk.zeuss@uni-marburg.de>
2021-06-22 12:19:55 +0200

    python env in start_DD_app.R

--- start_dd_app.R
+++ start_dd_app.R
@@ -1,8 +1,9 @@
-library(reticulate) # Python binding for R
-use_python(Sys.which("python")) # Set the path to a local python installation
+library(reticulate) # Python binding for R. If the package is not installed, execute 
+# install.packages("reticulate") # and hit Y when prompted for the miniconda installation
+use_python(Sys.which("python")) # Set the path to a local python installation.
 os <- import("os") # python module needed for managing files, folders and their paths
 py_install(packages = "opencv-python", pip = TRUE) # python module for computer vision
-py_install(packages = "pillow", pip = FALSE) # python module for ...
+py_install(packages = "pillow", pip = TRUE) # Python Imaging Library
 
 
 # Set the path to app.R for being able to execute the shiny app (runApp('app.R'))
