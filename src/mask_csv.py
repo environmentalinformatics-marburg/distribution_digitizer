3c68c6a469a1df4a75800279e5d90a75378b7f85
neelamsRules <82159079+neelamsRules@users.noreply.github.com>
2023-01-30 09:00:40 +0100

    added masking
    In this files filter the points on the map

---
+++ src/mask_csv.py
@@ -0,0 +1,46 @@
+import numpy as np
+import cv2
+import numpy as np
+from osgeo import gdal
+import csv
+import os
+import glob
+
+def geopointextract(tiffile, geofile,  outputcsv, n):
+   fields =[ "Filename", 'Centroid X', 'Centroid Y']
+   filename = outputcsv
+   img = gdal.Open(tiffile)
+   geoimg = gdal.Open(geofile)
+   gt = geoimg.GetGeoTransform()
+   img=np.array(img.GetRasterBand(1).ReadAsArray())
+   ret, thresh = cv2.threshold(img,120,255,cv2.THRESH_TOZERO_INV)
+   kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(n,n))
+   opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=3)
+   contours, hierarchy = cv2.findContours(opening, cv2.RETR_TREE,
+cv2.CHAIN_APPROX_SIMPLE)
+   with open(filename, 'a', newline='') as csvfile:
+   # creating a csv writer object
+     csvwriter = csv.writer(csvfile)
+# writing the fields
+     csvwriter.writerow(fields)
+     for c in contours:
+   # calculate moments for each contour
+      M = cv2.moments(c)
+   # calculate x,y coordinate of center
+      cX = int(M["m10"] / M["m00"])
+      cY = int(M["m01"] / M["m00"])
+      x_pixel = cX
+      y_line = cY
+      x_geo = gt[0] + x_pixel * gt[1] + y_line * gt[2]
+      y_geo = gt[3] + x_pixel * gt[4] + y_line * gt[5]
+      rows = [[tiffile, x_geo, y_geo]]
+# writing the data rows
+      csvwriter.writerows(rows)
+
+def maingeopointextract(workingDir,n):
+   inputdir = workingDir + "/data/output/georeferencing/"
+   outputcsv = workingDir + "/data/output/mask/georecords.csv"
+   geofiledir = workingDir + "data/templates/geopoints/"
+   for tiffile in glob.glob(inputdir +'*.tif'):
+    for geofile in glob.glob(geofiledir + '*.tif'):
+         geopointextract(tiffile, geofile, outputcsv, n)
---
+++ src/masking.py
@@ -0,0 +1,39 @@
+import PIL
+import numpy as np
+import cv2
+import os
+import glob
+from PIL import Image
+
+#source_python("D:/distribution_digitizer_students/src/masking.py")
+
+
+def geomask(file, outputdir, n):
+#create black and white masks
+  image = np.array(PIL.Image.open(file))
+  grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  ret, thresh = cv2.threshold(grayImage,125,255,cv2.THRESH_TOZERO_INV)
+  kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (n,n))
+  #kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (n,n))
+  #kernel1 = cv2.getStructuringElement(cv2.MORPH_CROSS, (n,n))
+  opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel1, iterations=3)
+  #closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel1, iterations=3)
+  #gradient = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel1)
+  (thr, blackAndWhiteImage) = cv2.threshold(opening, 0, 255, cv2.THRESH_BINARY_INV)
+  PIL.Image.fromarray(blackAndWhiteImage).save(os.path.join(outputdir, os.path.basename(file)))
+  indices = np.where(blackAndWhiteImage!= [0])
+  #coordinates = zip(indices[0], indices[1])
+  #print(coordinates)
+
+
+#maingeomask("D:/Results/", 5)
+workingDir="D:/Results/"
+#maingeomask(workingDir, 5)
+
+
+#def maingeomask(workingDir, n):
+inputdir = workingDir+"templates/"
+outputdir = workingDir+"masking/"
+os.makedirs(outputdir, exist_ok=True)
+for file in glob.glob(inputdir + '*.tif'):
+    geomask(file, outputdir, 5)
