36539e7d6fc7d802b65aff33d1d761ebcd172f51
forteva <forteva@staff.uni-marburg.de>
2023-12-11 15:53:33 +0100

    Add new fields to the config dialog. Add coments

--- app_write_config.R
+++ app_write_config.R
@@ -1,3 +1,45 @@
+# ============================================================
+# Script Author: [Spaska Forteva]
+# Created On: 2023-06-10
+# ============================================================
+
+# ============================================================
+# Config Dialog for Book Distribution Digitization
+# ============================================================
+# 
+# This configuration dialog is designed to gather essential 
+# information about a book before the digitization process. 
+# Users can input details to create a comprehensive summary 
+# for proper documentation. The dialog includes the following fields:
+# 
+# 1. Title: Enter the title of the book.
+# 2. Author: Provide the name of the book's author.
+# 3. Publication Year: Specify the year the book was published.
+# 4. Data Input Directory: Specify the directory where the raw 
+#    data for the book digitization is stored.
+# 5. Data Output Directory: Specify the directory where the 
+#    digitized output for the book will be stored.
+# 6. Number of Book Sites per One Print: Define the number of 
+#    book pages to be included in one printed output.
+# 7. All Printed Pages: Indicate whether all pages of the book 
+#     will be included in the digitization process.
+# 8. Site Number Position: Specify the placement of the site 
+#     number on each printed page (e.g., top-right, bottom-left).
+# 9. Image Format of the Scanned Sites: Choose the format 
+#     (e.g., JPEG, PNG) for the digitized pages.
+# 10. Page Color: Indicate the color of the book pages 
+#     (e.g., black and white, color).
+# 
+# This dialog aims to streamline the digitization process by 
+# ensuring that all relevant information is captured accurately. 
+# Once the user completes the form, the gathered details can 
+# be used for cataloging and organizing the digital version of 
+# the book, preserving its content for future reference.
+# ============================================================
+
+
+
+
 if(!require(magick)){
   install.packages("magick", dependencies = T)
   library(magick)
@@ -74,30 +116,42 @@ shinyApp(
                #Project directory
                p(paste0("Working directory - ",workingDir), style = "color:black"),
                
+               # Title: Provide the name of the book's author.
+               fluidRow(column(3,textInput("title", label="Title", value = "Title"))),
+                        
+               # Author: Provide the name of the book's author.
+               fluidRow(column(3,textInput("author", label="Author", value = "Autorname"))),
+              
+               # Publication Year: Specify the year the book was published.
+               fluidRow(column(3,textInput("pYear", label="Publication Year", value = "2023"))),
+               
                # Data input directory
-               fluidRow(column(3,textInput("dataInputDir", label=configStartDialog$i1, value = paste0(workingDir,"/data/input")))),
+               fluidRow(column(3,textInput("dataInputDir", label="Data input directory", value = paste0(workingDir,"/data/input")))),
                         
                # Data output directory
-               fluidRow(column(3,textInput("dataOutputDir", label=configStartDialog$i2, value =paste0(workingDir,"/data/output") ))),
+               fluidRow(column(3,textInput("dataOutputDir", label="Data output directory", value =paste0(workingDir,"/data/output")))),
                
                # numberSitesPrint
-               fluidRow(column(3,selectInput("numberSitesPrint", label=configStartDialog$i3,  c("One site per scan" = 1 ,"Two sites per scan"= 2)))),
+               fluidRow(column(3,selectInput("numberSitesPrint", label="Number of Book Sites per One Print",  c("One site per scan" = 1 ,"Two sites per scan"= 2)))),
                
                # allprintedPages
-               fluidRow(column(3,textInput("allPrintedPages", label=configStartDialog$i4, value = 100 ))),
+               fluidRow(column(3,textInput("allPrintedPages", label="All Printed Pages", value = 100 ))),
+               
+               # site number position
+               fluidRow(column(3,selectInput("sNumberPosition", label="Site Number Position", c("top"=1 , "botom"=2), selected=1 ))),
                
                # format;
-               fluidRow(column(3,selectInput("pFormat", label=configStartDialog$i5, c("tif"=1 , "png"=2, "jpg"=3), selected=1 ))),
+               fluidRow(column(3,selectInput("pFormat", label="Image Format of the Scanned Sites", c("tif"=1 , "png"=2, "jpg"=3), selected=1 ))),
                
                # Page color;
-               fluidRow(column(3,selectInput("pColor", label=configStartDialog$i6, c("black white"=1 , "color"=2), selected=1 ))),
+               fluidRow(column(3,selectInput("pColor", label="Page Color", c("black white"=1 , "color"=2), selected=1 ))),
                
-               # width;
-               #fluidRow(column(3,textInput("allprintedPages", label=configStartDialog$i4, value = config$allprintedPages ))),
+               # save the values in csv file
                fluidRow(column(3, actionButton("saveConfig",  label = "Save")))
             ) 
     )
   ),
+
   
   
   
@@ -114,17 +168,23 @@ shinyApp(
       
       x <- data.frame(workingDir= workingDir, 
                       workingDirInformation = "Your working directory is the local digitizer repository!",
+                      title = input$title,
+                      autor = input$author,
+                      pYear = input$pYear,
                       dataInputDir = input$dataInputDir,
                       dataOutputDir = input$dataOutputDir,
                       numberSitesPrint = input$numberSitesPrint,
                       allPrintedPages = input$allPrintedPages,
+                      sNumberPosition = input$sNumberPosition,
                       pFormat = input$pFormat,
                       pColor = input$pColor)
+      
       tf <- tempfile(fileext = ".csv")
       
+      print(paste("It were ",length(x), "fields information saved!"))
       ## To write a CSV file for input to Excel one might use
       write.table(x, file = paste0(workingDir,"/config/config.csv"), sep = ";", row.names = FALSE,
-                   quote=FALSE)
+                  quote=FALSE)
       
     })
     
