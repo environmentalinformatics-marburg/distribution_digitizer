4f02b74b064138239b1d1d5a83bb68f473c73ae6
LeaH <70723080+LeaHeidrich@users.noreply.github.com>
2025-01-27 10:49:55 +0100

    Update start_dd_app.R
    installation of needed packages

--- start_dd_app.R
+++ start_dd_app.R
@@ -1,10 +1,22 @@
 #-------------------------------------------------------------------------------------------------
 
-#use_python(Sys.which("python")) # Set the path to a local python installation.
+# Installation of needed packages (python) ####
+# deactivate using comment after first installation
+py_install(packages = "opencv", pip = FALSE) #used for map matching
+py_install(packages = "pillow", pip = FALSE)
+py_install(packages = "tesseract", pip = FALSE)
+py_install(packages = "pandas", pip = FALSE)
+py_install(packages = "GDAL", pip = FALSE)
+py_install(packages = "imutils", pip = FALSE)
+py_install(packages = "rasterio", pip = FALSE)
+py_install(packages = "geopandas", pip = FALSE)
 
+#use_python(Sys.which("python")) # Set the path to a local python installation.
 # use this row if you not use Anaconda or miniconda. the best way is to set the python system (environment)variables (Windows->system,...) 
+
 #use_python("C:/ProgramData/miniconda3/python.exe")
-os <- import("os") # python module needed for managing files, folders and their paths
+# os <- import("os") # python module needed for managing files, folders and their paths
+
 #py_install(packages = "osgeo", pip = FALSE)
 #py_install(packages = "opencv-python", pip = TRUE)
 #setwd("C:/ProgramData/Miniconda3/")
@@ -13,21 +25,22 @@ os <- import("os") # python module needed for managing files, folders and their 
 #py_install(packages = "pandas", pip = FALSE)
 #py_install(packages = "GDAL", pip = FALSE)
 
+
+#---------------------------------------------------------------------------------------------------
+
+# run this commands to load needed libraries
+library(reticulate) # Python binding for R. 
+library(shiny) # shiny library necessary for starting the app
+
+# install.packages("reticulate") # start the RStudio as admin and hit Y when prompted for the miniconda installation
 # Set the path to app.R for being able to execute the shiny app (runApp('app.R'))
 # By default, this app.R lies at the root of this repository. 
 # In RStudio, this path can be set automatically with
 # setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # or manually with a path of your choice:
-#---------------------------------------------------------------------------------------------------
-  
-library(reticulate) # Python binding for R. 
-# install.packages("reticulate") # start the RStudio as admin and hit Y when prompted for the miniconda installation
-
-setwd("D:/distribution_digitizer") # uncomment this line for setting the working directory manually.
-
+setwd("D:/distribution_digitizer") # adjust path as necessary
 getwd() # print the path to the working directory for copying into the Digitizer application (Field: "Working Directory").
 
-library(shiny) # shiny library necessary for starting the app
 # start this app to write the config file
 #runApp('app_write_config.R')
        
